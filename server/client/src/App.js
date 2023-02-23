757602a4c8bb45fc0234d0a2701e8215c5b3d564
quentinberry <34132543+quentinberry@users.noreply.github.com>
2023-02-23 22:17:18 +0100

    WIP

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 function App() {
+
+  const openLink = (site) => {
+    window.open(site);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Parallax pages={3}>
+        <ParallaxLayer offset={0} speed={0.9}>
+          <h1 class="indexTitle">Hi, I am Quentin Berger, a Berlin based Software Engineer.</h1>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5}>
+        <div class="memoji">
+            <div class="circle"></div>
+            <div class="memojiImage"></div>
+        </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.8}>
+          <h1 class='aboutMeTitle'>About me</h1>
+          <p class="text">
+            I fulfilled an apprenticeship at Zalando in Berlin. During that time I learned the principles of working in collaboration to create products for customers. 
+            <br /><br /> Since I started as a full-time Software Engineer in 2020, I have gathered experience with different languages, frameworks, best practices, and agile working methods. 
+            <br /><br /> Some of them being: <br /><b>Java, Spring Boot, JavaScript, React, Node.js, Express.js, JUnit, Jest, Git, PostgreSQL, and Go-Lang.</b>
+          </p>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5}>
+        <div class="memojiAboutMe">
+            <div class="circle"></div>
+            <div class="memojiImageAboutMe"></div>
+        </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2} speed={0.8}>
+          <h1 class="contactTitle">Contact me / My Socials</h1>
+          <div class="socialsList">
+              <div class="instagram"></div>
+              <div class="github"></div>
+              <div class="linkedin"></div>
+            </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2} speed={0.7}>
+        <div class="memojiContact">
+            <div class="circle"></div>
+            <div class="memojiImageContact"></div>
+        </div>
+        </ParallaxLayer>
+
+    </Parallax>
+    </div >
   );
 }
 
